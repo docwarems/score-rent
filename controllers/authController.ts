52c50e2ab611a5d0f4946274e1059493a02e920c
Michael Südkamp <ms2702@online.de>
2023-06-05 19:04:24 +0200

    Home and signup pages with W3 classes

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -9,22 +9,22 @@ const handleErrors = (err: any) => {
 
   // incorrect email
   if (err.message === 'incorrect email') {
-    errors.email = 'That email is not registered';
+    errors.email = 'Die E-Mail Adresse ist unbekannt';
   }
 
   // incorrect password
   if (err.message === 'incorrect password') {
-    errors.password = 'That password is incorrect';
+    errors.password = 'Das Passwort ist nicht korrekt';
   }
 
   // duplicate email error
   if (err.code === 11000) {
-    errors.email = 'that email is already registered';
+    errors.email = 'Diese E-Mail Adresse ist bereits in Verwendung';
     return errors;
   }
 
   // validation errors
-  if (err.message.includes('user validation failed')) {
+  if (err.message.includes('User validation failed')) {
     // console.log(err);
     Object.values(err.errors as { properties: any }).forEach(({ properties }) => {
       // console.log(val);
