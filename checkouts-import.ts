e3e5961887b646a0f245afd85720b12f4558656e
Michael Südkamp <ms2702@online.de>
2023-07-17 19:12:55 +0200

    checkin from checkout list generally working

--- checkouts-import.ts
+++ checkouts-import.ts
@@ -81,7 +81,7 @@ async function importCsv() {
             userId: user._id.toString(),
             scoreId,
             checkoutTimestamp: new Date("2020-01-01"),
-            checkinComment: record.comment,
+            checkoutComment: record.comment,
           });
           const checkouts = [];
           checkouts.push(checkout);
