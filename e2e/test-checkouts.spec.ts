55b12fad7397f9881cd3f5f1f0b77cbf9da5f731
Michael Südkamp <ms2702@online.de>
2023-09-30 15:40:07 +0200

    use local weberver for dev

--- e2e/test-checkouts.spec.ts
+++ e2e/test-checkouts.spec.ts
@@ -1,24 +1,26 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 require("dotenv").config();
 
-
 test.beforeEach(async ({ page }) => {
-    // login
-    await page.goto(`${process.env.CYCLIC_URL}/login`);
-    await page.locator('input[name="email"]').click();
-    await page.locator('input[name="email"]').fill(process.env.PLAYWRIGHT_USER!);
-    await page.locator('input[name="email"]').press("Tab");
-    await page
-      .locator('input[name="password"]')
-      .fill(process.env.PLAYWRIGHT_PASSWORD!);
-    await page.getByRole("button", { name: "Login" }).click();
-  await expect(page.getByRole('link', { name: 'Ausleihen anzeigen' })).toBeVisible();
+  // login
+  await page.goto(`/login`);
+  await page.locator('input[name="email"]').click();
+  await page.locator('input[name="email"]').fill(process.env.PLAYWRIGHT_USER!);
+  await page.locator('input[name="email"]').press("Tab");
+  await page
+    .locator('input[name="password"]')
+    .fill(process.env.PLAYWRIGHT_PASSWORD!);
+  await page.getByRole("button", { name: "Login" }).click();
+  await expect(
+    page.getByRole("link", { name: "Ausleihen anzeigen" })
+  ).toBeVisible();
 });
-  
-test('Show checkouts', async ({ page }) => {  
-    await page.getByRole('link', { name: 'Ausleihen anzeigen' }).click();
-  
-    await expect(page.getByRole('heading', { name: 'Ausleihen' })).toBeVisible();
-    await expect(page.getByRole('cell', { name: 'Orff De temporum fine comoedia' }).first()).toBeVisible();
-  });
-  
+
+test("Show checkouts", async ({ page }) => {
+  await page.getByRole("link", { name: "Ausleihen anzeigen" }).click();
+
+  await expect(page.getByRole("heading", { name: "Ausleihen" })).toBeVisible();
+  await expect(
+    page.getByRole("cell", { name: "Orff De temporum fine comoedia" }).first()
+  ).toBeVisible();
+});
--- e2e/test-login-page.spec.ts
+++ e2e/test-login-page.spec.ts
@@ -1,29 +1,32 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 require("dotenv").config();
 
-test('has title', async ({ page }) => {
-  await page.goto(`${process.env.CYCLIC_URL}/login`);
+test("has title", async ({ page }) => {
+  await page.goto(`/login`);
 
   await expect(page).toHaveTitle(/HSC Leihnoten Verwaltung/);
 });
 
-test('Password forgotten link', async ({ page }) => {
-  await page.goto(`${process.env.CYCLIC_URL}/login`);
+test("Password forgotten link", async ({ page }) => {
+  await page.goto(`/login`);
 
-  await page.getByRole('link', { name: 'Passwort vergessen' }).click();
+  await page.getByRole("link", { name: "Passwort vergessen" }).click();
 
-  await expect(page.getByRole('heading', { name: 'Passwort vergessen' })).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Passwort vergessen" })
+  ).toBeVisible();
 });
 
-test('Login link', async ({ page }) => {
-    await page.goto(`${process.env.CYCLIC_URL}/login`);
-    await page.locator('input[name="email"]').click();
-    await page.locator('input[name="email"]').fill(process.env.PLAYWRIGHT_USER!);
-    await page.locator('input[name="email"]').press("Tab");
-    await page
-      .locator('input[name="password"]')
-      .fill(process.env.PLAYWRIGHT_PASSWORD!);
-    await page.getByRole("button", { name: "Login" }).click();
-  await expect(page.getByRole('link', { name: 'QRCode für Login anzeigen' })).toBeVisible();
+test("Login link", async ({ page }) => {
+  await page.goto(`login`);
+  await page.locator('input[name="email"]').click();
+  await page.locator('input[name="email"]').fill(process.env.PLAYWRIGHT_USER!);
+  await page.locator('input[name="email"]').press("Tab");
+  await page
+    .locator('input[name="password"]')
+    .fill(process.env.PLAYWRIGHT_PASSWORD!);
+  await page.getByRole("button", { name: "Login" }).click();
+  await expect(
+    page.getByRole("link", { name: "QRCode für Login anzeigen" })
+  ).toBeVisible();
 });
-  
