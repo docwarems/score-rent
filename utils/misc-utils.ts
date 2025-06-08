e7f26433b63d1a34769627a411f3a51328282b70
Michael Südkamp <ms2702@online.de>
2025-06-08 13:47:36 +0200

    AWS SES

--- utils/misc-utils.ts
+++ utils/misc-utils.ts
@@ -1,23 +1,33 @@
 import nodemailer from "nodemailer";
-
+const { SESClient, SendRawEmailCommand } = require("@aws-sdk/client-ses");
+const { defaultProvider } = require("@aws-sdk/credential-provider-node");
 require("dotenv").config();
 
+const ses = new SESClient({
+  region: "eu-central-1",
+  credentialDefaultProvider: defaultProvider(),
+});
+
+// export const mailTransporter = nodemailer.createTransport({
+//     host: process.env.SMTP_HOST,
+//     port: parseInt(process.env.SMTP_PORT!),
+//     auth: {
+//       user: process.env.SMTP_USER,
+//       pass: process.env.SMTP_PASS,
+//     },
+//     greetingTimeout: 1000 * 10,
+//     logger:
+//       !!process.env.SMTP_DEBUG && process.env.SMTP_DEBUG.toLowerCase() == "true",
+//   });
+
 export const mailTransporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: parseInt(process.env.SMTP_PORT!),
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    },
-    greetingTimeout: 1000 * 10,
-    logger:
-      !!process.env.SMTP_DEBUG && process.env.SMTP_DEBUG.toLowerCase() == "true",
-  });
-  mailTransporter.verify(function (error, success) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("SMTP server is ready to take our messages");
-    }
-  });
-  
+  SES: { ses, aws: { SendRawEmailCommand } },
+});
+
+mailTransporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("SMTP server is ready to take our messages");
+  }
+});
