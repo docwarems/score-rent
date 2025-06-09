38d5e241a5278f0ce13c7e0092860ae081df6971
Michael Südkamp <ms2702@online.de>
2025-06-09 11:57:42 +0200

    wieder zurück zu altem Mail Provider wg. access-denied by SES

--- utils/misc-utils.ts
+++ utils/misc-utils.ts
@@ -3,25 +3,25 @@ const { SESClient, SendRawEmailCommand } = require("@aws-sdk/client-ses");
 const { defaultProvider } = require("@aws-sdk/credential-provider-node");
 require("dotenv").config();
 
-const ses = new SESClient({
-  region: "eu-central-1",
-  credentialDefaultProvider: defaultProvider(),
-});
+// const ses = new SESClient({
+//   region: "eu-central-1",
+//   credentialDefaultProvider: defaultProvider(),
+// });
 
 // export const mailTransporter = nodemailer.createTransport({
-//     host: process.env.SMTP_HOST,
-//     port: parseInt(process.env.SMTP_PORT!),
-//     auth: {
-//       user: process.env.SMTP_USER,
-//       pass: process.env.SMTP_PASS,
-//     },
-//     greetingTimeout: 1000 * 10,
-//     logger:
-//       !!process.env.SMTP_DEBUG && process.env.SMTP_DEBUG.toLowerCase() == "true",
-//   });
+//   SES: { ses, aws: { SendRawEmailCommand } },
+// });
 
 export const mailTransporter = nodemailer.createTransport({
-  SES: { ses, aws: { SendRawEmailCommand } },
+  host: process.env.SMTP_HOST,
+  port: parseInt(process.env.SMTP_PORT!),
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+  greetingTimeout: 1000 * 10,
+  logger:
+    !!process.env.SMTP_DEBUG && process.env.SMTP_DEBUG.toLowerCase() == "true",
 });
 
 mailTransporter.verify(function (error, success) {
