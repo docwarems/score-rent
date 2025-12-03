de7161b47c924c54f4d4e28a0390737f0ef4b201
Michael Südkamp <ms2702@online.de>
2025-12-03 21:25:54 +0100

    mongodump neu installiert

--- mongobackup.sh
+++ mongobackup.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-/opt/mongodb-tools/bin/mongodump --uri $1
+# Example (use quotes!!)
+# ./mongobackup.sh "mongodb+srv://... " "/mnt/c/temp"
+
+# Check if both parameters are provided
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <mongodb_uri> <output_directory>"
+    echo "Example: $0 'mongodb+srv://...' /mnt/c/temp"
+    exit 1
+fi
+
+~/opt/mongodb-tools/bin/mongodump --uri $1
 
 mv dump/prod dump/prod_20$(date +"%y%m%d")
 
