bb3535f99cb59092c02a836ea688ebb27ce88418
Michael Südkamp <ms2702@online.de>
2025-12-13 11:54:58 +0100

    issue #18 rename route score to admin

---
+++ dist/routes/adminRoutes.js
@@ -0,0 +1,73 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.admin = void 0;
+const { Router } = require("express");
+const scoreController = require("../controllers/scoreController");
+const { requireAuth, checkUser, requireUserVerified, requireAdmin, } = require("../middleware/authMiddleware");
+const score_utils_1 = require("../utils/score-utils");
+exports.admin = Router();
+// wildcard for all get/post actions
+exports.admin.get("*", requireAuth, requireAdmin);
+exports.admin.post("*", requireAuth, requireAdmin);
+exports.admin.get("/register", (req, res) => res.render("register-score", { scoreType: res.locals.scoreType }));
+exports.admin.post("/register", scoreController.register_score_post);
+exports.admin.get("/checkout", (req, res) => res.render("checkout", {
+    checkoutUser: res.locals.checkoutUser,
+    checkoutScore: res.locals.checkoutScore,
+    users: undefined,
+}));
+exports.admin.post("/checkout", scoreController.checkout_post);
+exports.admin.post("/updateCheckout", scoreController.updateCheckout_post);
+exports.admin.get("/checkin", (req, res) => res.render("checkin", {
+    checkinScore: res.locals.checkinScore,
+}));
+exports.admin.post("/checkin", scoreController.checkin_post);
+exports.admin.get("/checkouts", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    return res.render("checkouts", {
+        admin: true,
+        signatures: yield (0, score_utils_1.getScoreTypes)(),
+        filter: { signature: "", checkedOut: true },
+        checkouts: undefined,
+        error: undefined,
+    });
+}));
+exports.admin.post("/checkouts", scoreController.checkouts_post);
+exports.admin.post("/userSearch", scoreController.userSearch_post);
+exports.admin.get("/users", scoreController.users_get);
+exports.admin.post("/users", scoreController.users_post);
+exports.admin.post("/updateUser", scoreController.updateUser_post);
+exports.score.get("/history", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    return res.render("score-history", {
+        id: undefined,
+        checkouts: undefined,
+        error: undefined,
+    });
+}));
+exports.score.post("/history", scoreController.scoreHistory_post);
+exports.score.get("/vue-test", (req, res) => __awaiter(void 0, void 0, void 0, function* () { return res.render("vue-test"); }));
+exports.score.get("/vue-test.js", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    return res.render("vue-test.vue", {
+        age: 66,
+    });
+}));
+exports.score.get("/checkouts-vue", (req, res) => __awaiter(void 0, void 0, void 0, function* () { return res.render("checkouts-vue"); }));
+exports.score.get("/checkouts.js", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    return res.render("checkouts.vue", {
+        admin: true,
+        signatures: JSON.stringify(yield (0, score_utils_1.getScoreTypes)()),
+        filter: JSON.stringify({ signature: "", checkedOut: true }),
+        checkouts: undefined,
+        error: undefined,
+        hasError: false,
+    });
+}));
+exports.admin.post("/checkouts-vue", scoreController.checkouts_vue_post);
